d55da25be997c18925cd795809801e36626bab71
Swaby404 <swabyoliver@gmail.com>
2025-10-25 13:04:51 -0400

    added nav bar

---
+++ finance_school/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">  Home</a></li>
+        <li><a href="#AboutUs">About Us</a></li>
+        <li><a href="#ContactUs">Contact Us</a></li>
+      </ul>
+    </nav>
+  
+  )
+}
+export default Nav;
+ 
